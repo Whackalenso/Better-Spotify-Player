ba8b0a77f653fb4a3a6086c254f8775e7917e47e
Whackalenso <63932931+Whackalenso@users.noreply.github.com>
2023-05-03 23:31:41 -0700

    base

---
+++ web/src/App.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+function App() {
+  // useEffect(() => {
+  //   fetch("http://localhost:8888/login");
+  // }, []);
+
+  return <a href="http://localhost:8888/login">sign in with</a>;
+}
+
+export default App;
