eb782ffc445ac9df137744612ccb339071709e7c
Whackalenso <63932931+Whackalenso@users.noreply.github.com>
2023-05-07 22:46:42 -0700

    timestamp stuff

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,57 +1,234 @@
-import { useEffect, useState } from "react";
-import { PlayCircleFill, PauseCircleFill, ArrowClockwise, ArrowCounterclockwise, SkipStartFill, SkipEndFill } from "react-bootstrap-icons";
+import { useEffect, useState, useRef } from "react";
+import {
+  PlayCircleFill,
+  PauseCircleFill,
+  ArrowClockwise,
+  ArrowCounterclockwise,
+  SkipStartFill,
+  SkipEndFill,
+} from "react-bootstrap-icons";
 
-const api_url = "https://localhost:8888/"
+const api_url = "https://localhost:8888/";
 
 function App() {
   const [authenticated, setAuthenticated] = useState(true);
 
   useEffect(() => {
-    fetch(api_url + "is-authenticated").then((res) => res.json()).then((data) => {
-      setAuthenticated(data.authenticated);
-    });
+    // fetch(api_url + "is-authenticated")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     setAuthenticated(data.authenticated);
+    //   });
   }, []);
 
-  if (authenticated == null) { return <></>}
+  if (authenticated == null) {
+    return <></>;
+  }
 
-  return authenticated ? <Player/> : <a href={api_url + "login"}>Login in with Spotify</a>;
+  return authenticated ? (
+    <Player />
+  ) : (
+    <a href={api_url + "login"}>Login in with Spotify</a>
+  );
 }
+
+const msToTime = (ms) => {
+  var seconds = Math.floor(ms / 1000);
+  var minutes = Math.floor(seconds / 60);
+  var seconds = seconds % 60;
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  return `${minutes}:${seconds}`;
+};
 
 function Player() {
   const [currentSong, setCurrentSong] = useState({
-    album: {images: [{url: "https://upload.wikimedia.org/wikipedia/en/b/bc/AlbumcoverDaveHolland-WhatGoesAround.jpg"}]},
-    artists: [{name: "Dave Holland Big Band"}],
-    name: "Shadow Dance"
-  }) // TrackObject
+    album: {
+      images: [
+        {
+          url: "https://upload.wikimedia.org/wikipedia/en/b/bc/AlbumcoverDaveHolland-WhatGoesAround.jpg",
+        },
+      ],
+    },
+    artists: [{ name: "Dave Holland Big Band" }],
+    name: "Shadow Dance",
+    duration_ms: 884000,
+  }); // TrackObject
   const [playing, setPlaying] = useState(true);
-  const [progress, setProgress] = useState(0); // milliseconds
+  const [progress, setProgress] = useState(0.5); // percent
+  const [prevProgress, setPrevProgress] = useState(null);
 
-  return <div>
-    <div className="progress">
+  const [mousePos, setMousePos] = useState(0);
+  const mouseClick = useRef(false);
 
+  const [skipTimeInput, setSkipTimeInput] = useState(
+    localStorage.getItem("skipTime") ? localStorage.getItem("skipTime") : 5
+  );
+  const [skipTime, setSkipTime] = useState(skipTimeInput);
+
+  useEffect(() => {
+    localStorage.setItem("skipTime", skipTime);
+  }, [skipTime]);
+
+  function submitSkipTime() {
+    if (skipTimeInput > 0) {
+      setSkipTime(skipTimeInput);
+    } else {
+      setSkipTimeInput(skipTime);
+    }
+  }
+
+  function seek() {
+    console.log("seek");
+  }
+
+  function mouseUpdate(e, click = false) {
+    var pos =
+      Math.min(Math.max(e.clientX, 0), window.innerWidth) / window.innerWidth;
+    setMousePos(pos);
+
+    mouseClick.current = click;
+    if (e.buttons == 1) {
+      setProgress(pos);
+    }
+  }
+
+  const SkipBtn = (props) => (
+    <div className="skip-btn" onClick={props.onClick}>
+      <props.icon className="skip-arrow" size={40} />
+      <p className="skip-btn-num">{skipTime}</p>
     </div>
-    <div className="bottom-bar">
-      <div className="bottom-bar-content">
-        <div className="song-details-wrapper">
-          <img className="album-cover" src={currentSong.album.images[0].url}/>
-          <div className="song-details">
-            <div className="song-name">{currentSong.name}</div>
-            <div className="song-artist">{currentSong.artists[0].name}</div>
+  );
+
+  const timestampPos = (percent, pr = 50) => {
+    const px = percent * window.innerWidth;
+    if (px <= 60) {
+      return 10;
+    }
+    if (px >= window.innerWidth - pr) {
+      return window.innerWidth - pr - 50;
+    }
+    return `calc(${percent * 100}% - 50px)`;
+  };
+
+  const mouseTimestampStackPos = () => {
+    var mousePx = mousePos * window.innerWidth;
+    var progressPx = progress * window.innerWidth;
+    if (progressPx < 50) {
+      progressPx = 50;
+    }
+    if (progressPx > window.innerWidth - 50) {
+      progressPx -= 50;
+    }
+    if (mousePx >= progressPx - 40 && mousePx < progressPx + 40) {
+      return 40;
+    }
+    if (mousePx > window.innerWidth - 40) {
+      return 40;
+    }
+    return 10;
+  };
+
+  return (
+    <div className="player">
+      <div
+        className="progress-zone"
+        onMouseMove={mouseUpdate}
+        onMouseDown={(e) => {
+          e.preventDefault();
+          mouseUpdate(e, true);
+        }}
+        onMouseLeave={mouseUpdate}
+      >
+        <div
+          className="timestamp"
+          style={{
+            left: timestampPos(progress),
+            transitionDuration: mouseClick.current ? "100ms" : "0ms",
+          }}
+        >
+          {msToTime(progress * currentSong.duration_ms)}
+        </div>
+        <div className="timestamp" style={{ right: 10 }}>
+          {msToTime(currentSong.duration_ms)}
+        </div>
+        <div
+          className="timestamp mouse-timestamp"
+          style={{
+            left: timestampPos(mousePos, 0),
+            top: mouseTimestampStackPos(),
+            opacity:
+              msToTime(progress * currentSong.duration_ms) ===
+              msToTime(mousePos * currentSong.duration_ms)
+                ? 0
+                : 1,
+          }}
+        >
+          {msToTime(mousePos * currentSong.duration_ms)}
+        </div>
+        <div
+          className="scrubber"
+          style={{
+            left: `${mousePos * 100}%`,
+          }}
+        ></div>
+        {prevProgress ? (
+          <div className="scrubber" style={{ height: "5%" }}></div>
+        ) : null}
+        <div
+          className="progress"
+          style={{
+            width: `${progress * 100}%`,
+            transitionDuration: mouseClick.current ? "100ms" : "0ms",
+          }}
+        ></div>
+      </div>
+      <div className="bottom-bar">
+        <div className="bottom-bar-content">
+          <div className="song-details-wrapper">
+            <img
+              className="album-cover"
+              src={currentSong.album.images[0].url}
+            />
+            <div className="song-details">
+              <div className="song-name">{currentSong.name}</div>
+              <div className="song-artist">{currentSong.artists[0].name}</div>
+            </div>
           </div>
-        </div>
-        <div className="controls">
-          <SkipStartFill color="white" size={40}/>
-          <ArrowCounterclockwise color="white" size={40}/>
-          <PlayCircleFill color="white" size={40}/>
-          <ArrowClockwise color="white" size={40}/>
-          <SkipEndFill color="white" size={40}/>
-        </div>
-        <div className="options">
-          adfafd
+          <div className="controls">
+            <SkipStartFill className="control-btn" size={40} />
+            <SkipBtn icon={ArrowCounterclockwise} onClick={seek} />
+            <PlayCircleFill className="control-btn" size={40} />
+            <SkipBtn icon={ArrowClockwise} onClick={seek} />
+            <SkipEndFill className="control-btn" size={40} />
+          </div>
+          <div className="options">
+            <div className="options-item">
+              Skip amount (s)
+              <input
+                type="number"
+                min={1}
+                value={skipTimeInput}
+                onChange={(e) => {
+                  if (e.target.value < 1000) {
+                    setSkipTimeInput(e.target.value);
+                  }
+                }}
+                onBlur={submitSkipTime}
+                onKeyUp={(e) => {
+                  if (e.key == "Enter") {
+                    submitSkipTime();
+                    e.target.blur();
+                  }
+                }}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  );
 }
 
 export default App;
