2d9c20106ed90ea99cbacbfcee4f58cdcc9070b4
Whackalenso <63932931+Whackalenso@users.noreply.github.com>
2023-06-14 14:37:32 -0700

    try using proxy

--- api/index.js
+++ api/index.js
@@ -26,7 +26,6 @@ const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
-
 app.use(cors({credentials: true, origin: "https://better-spotify-player.vercel.app"}));
 
 function refreshToken(req, res) {
--- web/src/App.jsx
+++ web/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const [authenticated, setAuthenticated] = useState();
 
   useEffect(() => {
-    fetch(api_url + "is-authenticated", { credentials: "include" })
+    fetch("/is-authenticated")
       .then((res) => res.json())
       .then((data) => {
         setAuthenticated(data.authenticated);
